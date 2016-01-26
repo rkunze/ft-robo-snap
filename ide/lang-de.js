5ec8c5b19c7e5f92ff86a655e1456d26e54073dc
Richard Kunze <richard.kunze@web.de>
2016-01-26 03:33:07 +0100

    Add german translation.

---
+++ ide/lang-de.js
@@ -0,0 +1,61 @@
+/*
+ * lang-de.js: German translation for FT-Robo-Snap
+ *
+ * This file contains only translations for FT-Robo-Snap specific blocks
+ * and other elements. Translations for standard IDE elements are loaded from
+ * the original Snap! translation file
+ *
+ * Note to translators: This file has basically the same structure as a
+ * "standard" Snap! translation file, and the same rules and restritions for
+ * editing apply.
+ *
+ * (c) 2016 Richard Kunze
+ *
+ * This file is part of FT-Robo-Snap (https://github.com/rkunze/ft-robo-snap)
+ *
+ * FT-Robo-Snap is free software licensed under the Affero Gnu Public License,
+ * either version 3 or (at your discretion) any later version.
+ *
+ */
+
+/*global FTRoboSnap*/
+
+FTRoboSnap.dict.de = {
+
+    // Block labels
+    "$ftrobo set %ftroboOutput to %ftroboOutputValue":
+        "$ftrobo setze %ftroboOutput auf %ftroboOutputValue",
+
+    "$ftrobo run %ftroboMotor synchronized to %ftroboMotorOrNone %br at speed %ftroboMotorValue for %n steps":
+        "$ftrobo bewege %ftroboMotor synchron zu %ftroboMotorOrNone %br bei Tempo %ftroboMotorValue um %n Schritte",
+
+    "$ftrobo switch %ftroboInput is on?":
+        "$ftrobo Schalter %ftroboInput ist an?",
+
+    "$ftrobo motor %ftroboMotor is running?":
+        "$ftrobo Motor %ftroboMotor läuft?",
+
+    "$ftrobo current value of %ftroboCounter":
+        "$ftrobo Zählerstand von %ftroboCounter",
+
+    "$ftrobo current value of %ftroboInput":
+        "$ftrobo Wert von %ftroboInput",
+
+    // Menu items
+    // single output
+    "0 (off)": "0 (aus)",
+    "512 (max)": "512 (maximal)",
+
+    // motor outputs
+    "+512 (forward)" : "+512 (vorwärts)",
+    "0 (stop)": "0 (stop)",
+    "-512 (back)" : "-512 (rückwärts)",
+
+};
+
+
+// Translators: Do not edit anything below this line
+// ===========================================================================
+// Load the original translation file and monkey patch our additions
+// into the global dictionary
+FTRoboSnap.monkeyPatchTranslation(document.getElementById("language"));
